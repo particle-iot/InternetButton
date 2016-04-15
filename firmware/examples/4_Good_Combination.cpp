a44a0ec84f80e252fa7dd19ecf6fb2e4e4bcf2cb
Richard Whitney <jenesaisdiq@users.noreply.github.com>
2016-04-15 15:53:24 -0700

    spark -> particle

--- firmware/examples/4_Good_Combination.cpp
+++ firmware/examples/4_Good_Combination.cpp
@@ -19,7 +19,7 @@ void loop(){
     // If this calls for a full spectrum situation, let's go rainbow!
     if(b.allButtonsOn()) {
         // Publish the event "allbuttons" for other services like IFTTT to use
-        Spark.publish("allbuttons",NULL, 60, PRIVATE);
+        Particle.publish("allbuttons",NULL, 60, PRIVATE);
         b.rainbow(5);
         rainbow_mode = true;
 
@@ -38,7 +38,7 @@ void loop(){
     if (b.buttonOn(1)) {
         b.ledOn(12, 255, 0, 0); // Red
         // Publish the event "button1" for other services like IFTTT to use
-        Spark.publish("button1",NULL, 60, PRIVATE);
+        Particle.publish("button1",NULL, 60, PRIVATE);
         delay(500);
     }
     else {
@@ -48,7 +48,7 @@ void loop(){
     if (b.buttonOn(2)) {
         b.ledOn(3, 0, 255, 0); // Green
         // Publish the event "button2" for other services like IFTTT to use
-        Spark.publish("button2",NULL, 60, PRIVATE);
+        Particle.publish("button2",NULL, 60, PRIVATE);
         delay(500);
     }
     else {
@@ -58,7 +58,7 @@ void loop(){
     if (b.buttonOn(3)) {
         b.ledOn(6, 0, 0, 255); // Blue
         // Publish the event "button3" for other services like IFTTT to use
-        Spark.publish("button3",NULL, 60, PRIVATE);
+        Particle.publish("button3",NULL, 60, PRIVATE);
         delay(500);
     }
     else {
@@ -68,7 +68,7 @@ void loop(){
     if (b.buttonOn(4)) {
         b.ledOn(9, 255, 0, 255); // Magenta
         // Publish the event "button4" for other services like IFTTT to use
-        Spark.publish("button4",NULL, 60, PRIVATE);
+        Particle.publish("button4",NULL, 60, PRIVATE);
         delay(500);
     }
     else {
