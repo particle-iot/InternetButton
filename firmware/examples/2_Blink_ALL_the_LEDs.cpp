4272338f5c53f3c26575b7e03ad16cabf994c064
Richard Whitney <jenesaisdiq@users.noreply.github.com>
2016-04-15 15:51:50 -0700

    spark -> particle

--- firmware/examples/2_Blink_ALL_the_LEDs.cpp
+++ firmware/examples/2_Blink_ALL_the_LEDs.cpp
@@ -1,14 +1,14 @@
 #include "InternetButton/InternetButton.h"
 
-/* This SparkButton library has some useful functions.
+/* This InternetButton library has some useful functions.
 Here we blink ALL the LEDs instead of just one.*/
 
 InternetButton b = InternetButton();
 
 void setup() {
-    // Tell b to get everything ready to go
     // Use b.begin(1); if you have the original SparkButton, which does not have a buzzer or a plastic enclosure
     // to use, just add a '1' between the parentheses in the code below.
+    // Tell b to get everything ready to go
     b.begin();
 }
 
